a348308c02c8545ac2e73823265aa243aeb531f3
rajatsri94 <rajatsri94@gmail.com>
2025-08-17 12:31:00 +0000

    support for firebase studio

---
+++ .idx/dev.nix
@@ -0,0 +1,27 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.pnpm
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "pnpm"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
